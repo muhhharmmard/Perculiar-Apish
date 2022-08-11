b8e523bbb15b57534ff9a8d23578bed4abedfea4
me <culestfrosh@gmail.com>
2022-08-11 15:54:29 +0100

    added user model

--- Models/User.js
+++ Models/User.js
@@ -1,24 +1,27 @@
 import mongoose from 'mongoose'
 
-/* PetSchema will correspond to a collection in your MongoDB database. */
 const UserSchema = new mongoose.Schema({
-  fullname: {
-    /* The name of this user */
-
-    type: String,
-    required: [true, 'Please provide a name for this user.'],
-    maxlength: [60, 'Name cannot be more than 60 characters'],
-  },
+  
+  
+  name:{
+    type: String
+},
+  image:{
+    type: String
+},
   email:{
-    type:String,
-    required:[true,"Email is required"]
-  },
-  image_url: {
-    /* Url to user image */
-
-    required: [true, 'Please provide an image url for this user.'],
-    type: String,
-  }
+    type: String
+},
+  cart:{
+    type: Array
+},
+  wishlist:{
+    type: Array
+},
+  emailVerified:{
+    type: mongoose.Schema.Types.Mixed
+},
+  
 })
 
 export default mongoose.models.User || mongoose.model('User', UserSchema)
