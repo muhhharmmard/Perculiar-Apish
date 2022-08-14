4fb23994a20b8f9ba9efbaf8ab005971085e3dac
me <culestfrosh@gmail.com>
2022-08-14 18:30:08 +0100

    *updated edit file
    The formal edit file was just a clone of the old product edit,so i added some basic changes

--- pages/sell/edit.tsx
+++ pages/sell/edit.tsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
-import { useRouter } from 'next/router'
+import {
+  useState
+} from 'react'
+import {
+  useRouter
+} from 'next/router'
 import Link from 'next/link'
-import Loader from "../../../Components/Loader"
+import Loader from "../../Components/Loader"
 import {
   Typography,
   Card,
@@ -10,258 +14,231 @@ import {
 import {
   useEditProductMutation,
   useGetProductByIdQuery
-} from "../../../store/services/products"
+} from "../../store/services/products"
 
 const EdProductPage = () => {
   const router = useRouter();
-  const [message, setMessage]: string = useState('')
-  
+  const [message,
+    setMessage]: string = useState('wait for file upload before submiting')
+
   const {
-   data:product,
-   error:proErr,
-   isLoading:proLoad,
-   isSuccess:proScc
- } = useGetProductByIdQuery(router.query.id)
- product && {
-   const {data:pro} = product;
- const {
-   name,
-   price,
-   image_url,
-   tags,
-   department
- }= pro
-  const initialState={
-    name,
-    price,
-    image_url,
-    tags,
-    department,
-    owner:session.user.name
-  }
-  const edit = (e:React.MouseEvent<HTMLInputElement>)=>{
-    e.preventDefault();
-   useEditProductMutation(router.query.id,state);
-    router.push("/products")
-  }
-  const reducer = (state.action) =>{
-    switch (action.type) {
-      case 'setName':
-        return {
-          ...state,
-          name:action.payload
-        }
-        break;
-      case 'setPrice':
-        return {
-          ...state,
-          price:action.payload
-        }
-        break;
-      case 'setImage':
-       return {
-          ...state,
-          image_url:action.payload
-        }
-      case 'setTag':
-        return {
-          ...state,
-          tags:action.payload
-        }
-        break;
-      case 'setDept':
-        return {
-          ...state,
-          department:action.payload
-        }
-        break;
-      case 'reset':
-        return {
-         state:initialState
-        }
-        break;
-      
-      default:
-        return state
+    data: product,
+    error: proErr,
+    isLoading: proLoad,
+    isSuccess: proScc
+  } = useGetProductByIdQuery(router.query.id)
+  product && {
+    const {
+      data: pro
+    } = product;
+    const {
+      price,
+      image,
+      description,
+      category,
+      title
+    } = pro
+    const initialState = {
+      price,
+      image,
+      description,
+      category,
+      title
     }
-  }
-  const [state,setState] = useReducer(reducer,initialState)
-  const { data: session } = useSession()
-  const {
-   data,
-   error,
-   isLoading,
-   isSuccess
- } = useGetDeptsQuery();
-  const {
-   data:tags,
-   error:err,
-   isLoading:loading,
-   isSuccess:s
- } = useGetTagsQuery();
- 
- const departments=[]
- const tagS = []
- useEffect(()=>{
-   data && data.data.map(dept=>{
-      department.push({
-        value:dept,
-        label:dept.name
-      })
-   })
-   },[data])
-   
- useEffect(()=>{
-   tags && tags.data.map(dept=>{
-      tagS.push({
-        value:dept,
-        label:dept.name
-      })
-   })
-   },[tags])
-   
-  return(
-    <div>
+    const edit = (e: React.MouseEvent < HTMLInputElement >)=> {
+      e.preventDefault();
+      useEditProductMutation(router.query.id, state);
+      router.push("/sell/")
+    }
+
+    const [imageUpload,
+      setImageUpload] = useState(null);
+    const [imageUrl,
+      setImageUrl] = useState("");
+    const [msg,
+      setmsg] = useState("");
+    const [submittable,
+      setSubmittable] = useState(false)
+    const imagesListRef = ref(storage, "products/");
+    const uploadFile = () => {
+      if (imageUpload == null) return;
+      setmsg("Reading file...")
+      const imageRef = ref(storage, `products/${imageUpload.name + crypto.randomUUID()}`);
+      uploadBytes(imageRef, imageUpload).then((snapshot) => {
+        getDownloadURL(snapshot.ref).then((url) => {
+          setImage(url);
+          setmsg("File read")
+          setSubmittable(true)
+        });
+      });
+    };
+
+
+    const reducer = (state.action) => {
+      switch (action.type) {
+        case 'setName':
+          return {
+            ...state,
+            title: action.payload
+          }
+          break;
+        case 'setPrice':
+          return {
+            ...state,
+            price: action.payload
+          }
+          break;
+        case 'setImage':
+          return {
+            ...state,
+            image: action.payload
+          }
+        case 'setCat':
+          return {
+            ...state,
+            category: action.payload
+          }
+          break;
+        case 'setDesc':
+          return {
+            ...state,
+            description: action.payload
+          }
+          break;
+        case 'reset':
+          return {
+            state: initialState
+          }
+          break;
+
+        default:
+          return state
+        }
+      }
+      const [state,
+        setState] = useReducer(reducer, initialState)
+      const {
+        data: session
+      } = useSession()
+
+      return(
+        <div>
       <Box
-      component="form"
-      sx={{
-        '& .MuiTextField-root': { m: 1, width: '25ch' },
-      }}
-      noValidate
-      autoComplete="off"
-    >
+          component="form"
+          sx={ {
+            '& .MuiTextField-root': { m: 1, width: '25ch' },
+          }}
+          noValidate
+          autoComplete="off"
+          >
     <TextField
-    id="product-name"
-    label="Product Name"
-    placeholder="Product Name"
-    className="w-full roundedprops" 
-     InputProps={{
-     value:state.name,
-     onChange:(e:React.ChangeEvent<HTMLInputElement>)=>{
-       setState({
-       type:"setName",
-       payload:e.target.value
-       })
-     },
-    
-       variant:"standard"
-     }}
-     />
-       <TextField
-    id="image_url"
-    label="image_url"
-    placeholder="Product Image"
-    className="w-full roundedprops" 
-     InputProps={{
-     value:state.image_url,
-     onChange:(e:React.ChangeEvent<HTMLInputElement>)=>{
-       setState({
-       type:"setImage",
-       payload:e.target.value
-       })
-     },
-    
-       variant:"standard"
-     }}
-     />
+            id="product-name"
+            label="Product Name"
+            placeholder="Product Name"
+            className="w-full roundedprops"
+            InputProps={ {
+              value: state.title,
+              onChange: (e: React.ChangeEvent < HTMLInputElement >)=> {
+                setState({
+                  type: "setName",
+                  payload: e.target.value
+                })
+              },
+
+              variant: "standard"
+            }}
+            />
+    <TextField
+            id="product-name"
+            label="Product Description"
+            placeholder="Product Description"
+            className="w-full roundedprops"
+            InputProps={ {
+              value: state.description,
+              onChange: (e: React.ChangeEvent < HTMLInputElement >)=> {
+                setState({
+                  type: "setDesc",
+                  payload: e.target.value
+                })
+              },
+
+              variant: "standard"
+            }}
+            />
+    <TextField
+            id="product-category"
+            label="Product Category"
+            placeholder="Product Category"
+            className="w-full roundedprops"
+            InputProps={ {
+              value: state.title,
+              onChange: (e: React.ChangeEvent < HTMLInputElement >)=> {
+                setState({
+                  type: "setCat",
+                  payload: e.target.value
+                })
+              },
+
+              variant: "standard"
+            }}
+            />
             <TextField
-    id="image_url"
-    label="image_url"
-    placeholder="Product Image"
-    className="w-full roundedprops" 
-     InputProps={{
-     type:file,
-     value:state.image_url,
-     onChange:(e:React.ChangeEvent<HTMLInputElement>)=>{
-       setState({
-       type:"setImage",
-       payload:e.target.value
-       })
-     },
-    
-       variant:"standard"
-     }}
-     />
-     
+            id="image_url"
+            label="image_url"
+            placeholder="Product Image"
+            className="w-full roundedprops"
+            InputProps={ {
+              type: file,
+              value: state.image,
+
+              onChange: (e: React.ChangeEvent < HTMLInputElement >)=> {
+                setImageUpload(e.target.files[0])
+              },
+
+              variant: "standard"
+            }}
+            />
+     {msg}
+     <Button onClick={e=>uploadFile()}>
+     <Typography variant="h5">
+     Read Image
+     </Typography>
+     </Button>
        <TextField
-    id="product-price"
-    label="Product Price"
-    placeholder="Product Price"
-    className="w-full roundedprops" 
-     InputProps={{
-     type:number,
-     value:state.price,
-     onChange:(e:React.ChangeEvent<HTMLInputElement>)=>{
-       setState({
-       type:"setPrice",
-       payload:e.target.value
-       })
-     },
-    
-       variant:"standard"
-     }}
-     />
-    
-     <TextField
-          id=""
-          select
-          label="Tag"
-          value={state.tags}
-           InputProps={{
-             multiple:true
-           }}
-          onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
-            setState({
-              type:"setTag",
-              payload:e.target.value
-            })
-          }}
-          helperText="Please select a tag"
-        >
-          {tagS.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-        <TextField
-          id=""
-          select
-          label="Department"
-          value={state.department}
-           onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
-            setState({
-              type:"setDept",
-              payload:e.target.value
-            })
-          }}
-     
-          helperText="Please select a department"
-        >
-          {departments.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-     <Button type='submit' onClick="edit">
+            id="product-price"
+            label="Product Price"
+            placeholder="Product Price"
+            className="w-full roundedprops"
+            InputProps={ {
+              type: number,
+              value: state.price,
+              onChange: (e: React.ChangeEvent < HTMLInputElement >)=> {
+                setState({
+                  type: "setPrice",
+                  payload: e.target.value
+                })
+              },
+
+              variant: "standard"
+            }}
+            />
+     <Button type='submit' onClick="edit" disabled={submitable ? false : true}>
      <Typography variant="h3">
      Done
      </Typography>
      </Button>
     </Box>
-    </div>
+        </div>
+      )
+
+    }
+    return (
+      <>
+      <Loader /> < />
     )
-  
- }
- return (
-   <>
-   <Loader/>
-   </>
-   )
 
-}
+  }
 
 
 
-export default EdProductPage
+  export default EdProductPage
