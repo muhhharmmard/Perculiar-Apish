331aa8b37478848d9c05e51484aa657d2ee8f5ea
me <culestfrosh@gmail.com>
2022-08-19 12:40:34 +0100

    Added a theme Context
    Added to two themes,dark and light to the app context

--- store/context.tsx
+++ store/context.tsx
@@ -1,34 +1,90 @@
-import { createContext, useState, useContext, useEffect } from "react";
+import {
+  createContext,
+  useState,
+  useContext,
+  useEffect,
+  useRef
+} from "react";
 
-import { useRouter } from "next/route";
-import { useGetAllProductsQuery } from "./services/products"; /*
-import { 
+import {
+  useRouter
+} from "next/route";
+import {
+  useGetAllProductsQuery
+} from "./services/products"; /*
+import {
   useGetAllUsersQuery,
-  
 } from "./services/users";*/
 
-import { useSession } from "next-auth/react";
+import {
+  useSession
+} from "next-auth/react";
 import Loader from "../Components/Loader";
-import { Typography } from "@mui/material";
+import {
+  Typography
+} from "@mui/material";
 
 const AppContext = createContext();
 
-const AppWrapper = ({ children }) => {
-  const { datum } = useSession();
-  const [useR, setUseR] = useState(null);
-  const [value, setValue] = useState({
-    user: {},
-    products: [],
-    categories: [],
-  });
+function usePrevious(theme) {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = theme
+  })
+  return ref.current
+}
 
-  const { data, error, isLoading, isSuccess } = useGetAllProductsQuery();
+const AppWrapper = ({
+  children
+}) => {
+  const {
+    datum
+  } = useSession();
+  const [useR,
+    setUseR] = useState(null);
+  const [value,
+    setValue] = useState({
+      user: {},
+      products: [],
+      categories: [],
+    });
+
+  const {
+    data,
+    error,
+    isLoading,
+    isSuccess
+  } = useGetAllProductsQuery();
   useEffect(() => {
     if (datum) {
       setUseR(datum.user);
     }
-  }, [datum]);
-  const filter = (prods, categories) => {};
+  },
+    [datum]);
+  const filter = (prods,
+    categories) => {};
+
+  // defaults to light theme
+  const [theme, setTheme] = useState('light')
+
+  // set user's preferred color scheme on first load
+  useLayoutEffect(() => {
+    setTheme(
+      !!window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+      ? 'dark': 'light'
+    )
+  },
+    [])
+
+  // change theme
+  const oldTheme = usePrevious(theme)
+  useLayoutEffect(() => {
+    document.documentElement.classList.remove(`theme-${oldTheme}`)
+    document.documentElement.classList.add(`theme-${theme}`)
+  },
+    [theme,
+      oldTheme])
+
   useEffect(() => {
     if (data) {
       const prods = data.data;
@@ -55,27 +111,33 @@ const AppWrapper = ({ children }) => {
       }
       const categoriesWithProducts = allCats;
 
+  function toggleTheme() {
+    if (theme === 'light') setTheme('dark')
+    else setTheme('light')
+  }
+
       setValue({
         user: useR,
         products: prods,
         categories: new Set(categories),
         categoriesWithProducts: categoriesWithProducts,
+        theme:theme,
+        toggleTheme:toggleTheme
       });
     }
-  }, [data]);
+  },
+    [data]);
   if (error) {
     error.data.isLoggedIn && router.push("/login");
     return <Typography variant="h1"></Typography>;
   }
-  if (isLoading) {
-  }
-  if (data) {
-  }
+  if (isLoading) {}
+  if (data) {}
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export default AppWrapper;
 
-export function useAppContext() {
-  return useContext(AppContext);
-}
+  export function useAppContext() {
+    return useContext(AppContext);
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,8 @@ module.exports = {
   content: [
     "./pages/**/*.{ts,tsx}",
     "./pages/products/**/*.{ts,tsx}",
+    "./pages/categories/**/*.{ts,tsx}",
+    "./pages/sell/**/*.{ts,tsx}",
     "./Components/**/*.{ts,tsx}",
   ],
   theme: {
